2518bc7f9b9bca0a3adc46953610d19d08e9a1b3
Arun <arunkottooli@hotmail.com>
2011-02-08 20:21:41 +0530

    completed crud

--- apps/frontend/modules/pages/templates/indexSuccess.php
+++ apps/frontend/modules/pages/templates/indexSuccess.php
@@ -5,14 +5,16 @@
 <table id="items" class="tablesorter">
   <thead>
     <tr>
-        <th><?php echo ''; ?></th>
+        <th><?php echo 'page'; ?></th>
+        <th><?php echo 'body'; ?></th>
         <th></th>
     </tr>
   </thead>
   <tbody>
     <?php foreach ($pages as $row) { ?>
       <tr>
-          <td><?php echo ''; ?></td>
+          <td><?php echo $row->getName(); ?></td>
+          <td><?php echo $row->getBody(); ?></td>
           <td><a href="<?php echo url_for('pages_edit', $row); ?>"><?php echo __("Edit"); ?></a> <?php echo link_to('Delete', '@pages_delete?id='.$row->getId(), array('method' => 'delete', 'confirm' => __('Are you sure?'), 'class' => 'delete')); ?></td>
       </tr>
     <?php } ?>
