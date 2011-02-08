2b5d160d2e19b8af19d5d3586332781a702bc03b
Arun <arunkottooli@hotmail.com>
2011-02-08 19:55:23 +0530

    schema created

---
+++ lib/filter/doctrine/pageFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * page filter form.
+ *
+ * @package    crud
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pageFormFilter extends BasepageFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/pageForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * page form.
+ *
+ * @package    crud
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pageForm extends BasepageForm
+{
+  public function configure()
+  {
+  }
+}
